5eecd8cfc66b197063323bd2e37df25cd2d8f861
moulyaklnsp <klnsmoulya@gmail.com>
2025-07-04 19:14:53 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { DatabaseProvider } from './contexts/DatabaseContext';
 import { WalletProvider } from './contexts/WalletContext';
 import { AuthProvider } from './contexts/AuthContext';
